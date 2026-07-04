a61859f66f7157dfd8a1f77f1aa639bfa405850c
Logurick <mgsft111@gmail.com>
2026-07-04 16:30:30 +0900

    管理画面アイコンフォントを有効化

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,9 @@
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+
 import { createApp } from 'vue'
 import { createVuetify } from 'vuetify'
+
 import App from './App.vue'
 
 createApp(App).use(createVuetify()).mount('#app')
